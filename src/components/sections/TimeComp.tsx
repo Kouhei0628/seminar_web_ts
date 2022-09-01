218534a163083bc279904c8a2183c5c3819e13e9
Kouhei <{mkkhiti.iltill@gmail.com}>
2022-09-01 22:28:31 +0900

    improve timecomp

--- src/components/sections/TimeComp.tsx
+++ src/components/sections/TimeComp.tsx
@@ -7,25 +7,19 @@ const TimeComp: React.FC = () => {
     const deadline: Date = new Date(2022, 10 - 1, 17, 0);
     const now: Date = new Date();
     const diff: number = deadline.getTime() - now.getTime();
-    const d = Math.floor(diff / (1000 * 60 * 60 * 24)).toString();
-    const h = Math.floor(
-      (diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
-    ).toString();
-    const m = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60)).toString();
+    const d = Math.floor(diff / (1000 * 60 * 60 * 24));
+    const h = Math.floor((diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const m = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
     if (!time) {
       setTime(` ${d} 日 ${h} 時間 ${m} 分`);
     }
     const timer = window.setInterval(() => {
-      const _deadline = new Date(2022, 10 - 1, 17, 0);
-      const _now = new Date();
-      const _diff = _deadline.getTime() - _now.getTime();
-      const _d = Math.floor(_diff / (1000 * 60 * 60 * 24)).toString();
-      const _h = Math.floor(
-        (_diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
-      ).toString();
-      const _m = Math.floor(
-        (_diff % (1000 * 60 * 60)) / (1000 * 60)
-      ).toString();
+      const _deadline: Date = new Date(2022, 10 - 1, 17, 0);
+      const _now: Date = new Date();
+      const _diff: number = _deadline.getTime() - _now.getTime();
+      const _d = Math.floor(_diff / (1000 * 60 * 60 * 24));
+      const _h = Math.floor((_diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+      const _m = Math.floor((_diff % (1000 * 60 * 60)) / (1000 * 60));
       setTime(` ${_d} 日 ${_h} 時間 ${_m} 分`);
     }, 1000);
     return () => window.clearInterval(timer);
