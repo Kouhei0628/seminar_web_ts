97f6d4a58ad68ad53dca27f4f02768dfa43f617c
Kouhei <{mkkhiti.iltill@gmail.com}>
2022-09-01 15:39:41 +0900

    type of coats n colors declared

--- src/data/coats.ts
+++ src/data/coats.ts
@@ -1,22 +1,28 @@
-const thieves = [{
-        id: 1,
-        name: "イグニスアーラ",
-        alt: "イグニスアーラの紋章",
-    },
-    {
-        id: 2,
-        name: "アルマガスト",
-        alt: "アルマガストの紋章",
-    },
-    {
-        id: 3,
-        name: "マレスアルカナ",
-        alt: "マレスアルカナの紋章",
-    },
-    {
-        id: 4,
-        name: "テレノヴェリア",
-        alt: "テレノヴェリアの紋章",
-    },
+type Coats = {
+  id: number;
+  name: string;
+  alt: string;
+}[];
+const thieves: Coats = [
+  {
+    id: 1,
+    name: "イグニスアーラ",
+    alt: "イグニスアーラの紋章",
+  },
+  {
+    id: 2,
+    name: "アルマガスト",
+    alt: "アルマガストの紋章",
+  },
+  {
+    id: 3,
+    name: "マレスアルカナ",
+    alt: "マレスアルカナの紋章",
+  },
+  {
+    id: 4,
+    name: "テレノヴェリア",
+    alt: "テレノヴェリアの紋章",
+  },
 ];
 export default thieves;
